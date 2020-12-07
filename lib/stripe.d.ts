1daa8322b4ef667d610929f133779aa71fb9703a
zeus <jeremy@zeus.dev>
2020-12-06 19:35:31 -0500

    Removed lib folder from repo

--- lib/stripe.d.ts
+++
@@ -1,5 +0,0 @@
-import 'isomorphic-fetch';
-import { CardDetails } from './types/CardDetails';
-import { TokenResponse } from './types/TokenResponse';
-export declare const STRIPE_URL = "https://api.stripe.com/v1/tokens";
-export declare function createToken(publicKey: string, cardDetails: CardDetails): Promise<TokenResponse>;
